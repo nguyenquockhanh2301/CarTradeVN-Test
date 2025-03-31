505fd8263d1dcb55418b67560e07f06cc7efe8b3
ThaiChuT9 <iamtk4i@gmail.com>
2025-04-01 00:03:14 +0700

    first commit

---
+++ src/main/java/com/cartradevn/cartradevn/CarTradeVnApplication.java
@@ -0,0 +1,13 @@
+package com.cartradevn.cartradevn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarTradeVnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarTradeVnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cartradevn/cartradevn/CarTradeVnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartradevn.cartradevn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarTradeVnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
